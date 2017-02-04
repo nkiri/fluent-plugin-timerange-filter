84bbd136dca7085dae174625c38e2ce836886acb
nkiri <naokiryu@gmail.com>
2017-02-04 11:48:41 +0900

    init

---
+++ lib/fluent/plugin/filter_timerange.rb
@@ -0,0 +1,31 @@
+class Fluent::Plugin::TimerangeFilter < Fluent::Plugin::Filter
+	Fluent::Plugin.register_filter('timerange', self)
+
+	config_param :starttime, :string
+	config_param :endtime, :string
+
+	def start
+		super
+	end
+
+	def shutdown
+		super
+	end
+
+	def configure(conf)
+		super
+		@starttime = Time.parse('1970-01-01 ' << conf['starttime'])
+		@endtime = Time.parse('1970-01-01 ' << conf['endtime'])
+	end
+
+	def filter(tag, time, record)
+		@now = Time.parse('1970-01-01 ' << Time.at(time).strftime('%R'))
+		if @now > @starttime and @now < @endtime
+			return record
+		else
+			return nil
+		end
+	end
+
+end
+
